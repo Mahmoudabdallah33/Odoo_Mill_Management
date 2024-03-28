078b66577d0c52b9ca3f8d2187f5eab43c059fc5
Mahmoudabdallah33 <153030672+Mahmoudabdallah33@users.noreply.github.com>
2024-03-28 22:05:03 +0200

    Add files via upload

---
+++ Mill/__manifest__.py
@@ -0,0 +1,25 @@
+
+{
+    'name' : 'Mill',
+    'version' : '1.2',
+    'summary': ' ',
+    'sequence': 0,
+    'description': """ ***  """,
+    'category': 'Mill_management',
+    'website': ' ',
+    'depends' : ['sale', 'product','mail',],
+    'data': [
+        'security/ir.model.access.csv',
+        'security/security_group.xml',
+        'data/source_seq.xml',
+        'views/menu.xml',
+        'views/sources.xml',
+    ],
+    'demo': [     ],
+    'installable': True,
+    'application': True,
+    'assets': {
+
+    },
+    'license': 'LGPL-3',
+}
